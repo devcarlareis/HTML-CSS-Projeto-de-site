c63d637acd706510373c94507a5d2e5d78771f64
devcarlareis <carla.jusreis@gmail.com>
2022-10-26 17:52:34 -0300

    navbar mobile e config do menu para mobile

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log('funcionou')
+//instrução para utilizar o jquery quando o documento estiver pronto, o html no caso.
+//açoes
+$(document).ready(function() {
+
+    //var recebe um seletor de classe que vai mapear o botão, temos o botão
+    var btnMenuMobile = $('.btn-menumobile')
+
+    //adicionando evento de click
+    $(btnMenuMobile).on('click', function() {
+
+        //quando o botão for clickado a class será mudada
+        //insere a class se não tem, tira se tem
+        $('.nav-container ul').toggleClass('open');
+    })
+})
